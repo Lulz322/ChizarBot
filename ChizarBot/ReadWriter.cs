87675b81dfc88f5bae0ac479acc6a368b32ca387
Windows <freddyzet@gmail.com>
2020-06-06 23:22:54 +0300

    ???

--- ChizarBot/ReadWriter.cs
+++ ChizarBot/ReadWriter.cs
@@ -8,7 +8,7 @@ namespace ChizarBot
     public class ReadWriter
     {
 
-        public static List<ulong> TakeList(string path)
+        public static List<ulong> TakeUintList(string path)
         {
             List<ulong> FromFile = new List<ulong>();
             try
@@ -17,6 +17,26 @@ namespace ChizarBot
                 {
                     while (!sr.EndOfStream)
                         FromFile.Add(Convert.ToUInt64(sr.ReadLine()));
+                    sr.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            return FromFile;
+        }
+        public static List<string> TakeStringList(string path)
+        {
+            List<string> FromFile = new List<string>();
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while (!sr.EndOfStream)
+                        FromFile.Add(sr.ReadLine());
                     sr.Close();
                 }
 
